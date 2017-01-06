8b095ffbd2925d4e406bde189de09f78fee44c01
Tuyen Nguyen <nntuyen92@gmail.com>
2017-01-06 12:01:34 +0700

    Do a sample

--- sample/src/main/java/com/tuyenmonkey/sample/MainActivity.java
+++ sample/src/main/java/com/tuyenmonkey/sample/MainActivity.java
@@ -1,9 +1,7 @@
 package com.tuyenmonkey.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.tuyenmonkey.AutoFillEditText;
-import java.util.Arrays;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    AutoFillEditText editText = (AutoFillEditText)findViewById(R.id.afetEmail);
-    editText.addSuggestions(Arrays.asList("0935939393", "0961342123"));
+    //AutoFillEditText editText = (AutoFillEditText)findViewById(R.id.afetEmail);
+    //editText.addSuggestions(Arrays.asList("0935939393", "0961342123"));
   }
 }
